2a4f8e27814c16c7ac0fc95f89d36ed2e4b6993f
alestucci <alessandrotucci@gmail.com>
2022-03-04 14:31:11 +0100

    Html objects, css styles, js algorithm

---
+++ script.js
@@ -0,0 +1,7 @@
+//Click sul bottone crea 5 numeri casuali (da 1 a 100??) non ripetuti
+
+//i numeri vengono mostrati a video per 30 secondi (le prove le faremo con 3 secondi)
+
+// dopo i 30 secondi i numeri scompaiono e compaiono 5 campi per l'inserimento dei numeri
+
+//dopo aver inserito i 5 numeri il programma controlla quanti e quali siano esatti
